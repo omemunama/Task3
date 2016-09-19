13b2779a1684af81fc40f9f7cb0789b71015c24a
omemunama <xnursx@gmail.com>
2016-09-19 22:26:56 +0700

    register layout & welcome layout

--- app/src/main/java/nursulaeman/task3/MainActivity.java
+++ app/src/main/java/nursulaeman/task3/MainActivity.java
@@ -1,8 +1,11 @@
 package nursulaeman.task3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        TextView txtView = (TextView) findViewById(R.id.tv_reg);
+        txtView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    */
     }
 }
