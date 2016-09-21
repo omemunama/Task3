e781686415cb191d06235c9ce6cbf0c73f8546f0
omemunama <xnursx@gmail.com>
2016-09-21 14:47:56 +0700

    use AwesomeValidation email input

--- app/src/main/java/nursulaeman/task3/MainActivity.java
+++ app/src/main/java/nursulaeman/task3/MainActivity.java
@@ -5,10 +5,18 @@ import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
+
 public class MainActivity extends AppCompatActivity {
+
+    AwesomeValidation mAwesomeValidation = new AwesomeValidation(BASIC);
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv_reg = (TextView) findViewById(R.id.tv_reg);
         tv_reg.setPaintFlags(tv_reg.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        mAwesomeValidation.addValidation(MainActivity.this, R.id.et_log_1, Patterns.EMAIL_ADDRESS, R.string.err_email);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAwesomeValidation.validate();
+            }
+        });
 
     }
 
--- app/src/main/java/nursulaeman/task3/RegisterActivity.java
+++ app/src/main/java/nursulaeman/task3/RegisterActivity.java
@@ -2,12 +2,31 @@ package nursulaeman.task3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    AwesomeValidation mAwesomeValidation = new AwesomeValidation(BASIC);
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAwesomeValidation.addValidation(RegisterActivity.this, R.id.et_reg_1, Patterns.EMAIL_ADDRESS, R.string.err_email);
+        btn_register = (Button) findViewById(R.id.btn_reg);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAwesomeValidation.validate();
+            }
+        });
+
     }
 }
