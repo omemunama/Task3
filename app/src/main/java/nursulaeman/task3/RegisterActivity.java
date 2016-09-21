5cf7e0d794f62cf1593433547fca539155cab50f
omemunama <xnursx@gmail.com>
2016-09-21 15:53:52 +0700

    use validation email

--- app/src/main/java/nursulaeman/task3/RegisterActivity.java
+++ app/src/main/java/nursulaeman/task3/RegisterActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
+
 import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -20,13 +23,35 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mAwesomeValidation.addValidation(RegisterActivity.this, R.id.et_reg_1, Patterns.EMAIL_ADDRESS, R.string.err_email);
+        final EditText pass1 = (EditText) findViewById(R.id.et_reg_2);
+        final EditText pass2 = (EditText) findViewById(R.id.et_reg_3);
         btn_register = (Button) findViewById(R.id.btn_reg);
+
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAwesomeValidation.validate();
+                if (!validatePass1(pass1.getText().toString())) {
+                    pass1.setError("Invalid Password");
+                    pass1.requestFocus();
+                } else if (!pass2.equals(pass1)) {
+                    pass2.setError("Invalid Password Confirmation");
+                    pass2.requestFocus();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Input Success", Toast.LENGTH_LONG).show();
+                }
             }
         });
-
     }
+
+    protected boolean validatePass1(String pass1) {
+        if (pass1 != null && pass1.length() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
+
+
