a19a12701a47a9dd60964c777c5d8e8e6ecfa77a
omemunama <xnursx@gmail.com>
2016-09-21 20:48:13 +0700

    press back twice to exit

--- app/src/main/java/nursulaeman/task3/MainActivity.java
+++ app/src/main/java/nursulaeman/task3/MainActivity.java
@@ -2,6 +2,7 @@ package nursulaeman.task3;
 
 import android.content.Intent;
 import android.graphics.Paint;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +10,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     AwesomeValidation mAwesomeValidation = new AwesomeValidation(BASIC);
     Button btn_login;
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,4 +46,24 @@ public class MainActivity extends AppCompatActivity {
         i.setClass(this,RegisterActivity.class);
         startActivity(i);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
 }
